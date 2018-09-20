b5c49119f6318d1ec73b1d2bb3bf9180d80204ab
Andrew <andrewmd4774@gmail.com>
2018-09-19 21:47:42 -0400

    Schema created

---
+++ models/articles.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    link: {
+        type: String,
+        required: true
+    },
+
+    summary: {
+        type: String,
+        required: true
+    },
+
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "note"
+    }
+});
+
+var article = mongoose.model("article", articleSchema);
+
+module.exports = article;
