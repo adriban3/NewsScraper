9e64d2b7ff86be185648713669c6e81a6b28fc7f
Andrew <andrewmd4774@gmail.com>
2018-09-20 20:41:27 -0400

    break over

---
+++ public/app.js
@@ -0,0 +1,23 @@
+$(".home").on("click", function (event) {
+
+});
+
+$(".scrape").on("click", function (event) {
+
+});
+
+$(".saved").on("click", function (event) {
+
+});
+
+$(".clear").on("click", function (event) {
+
+});
+
+$(".note").on("click", function (event, this) {
+
+});
+
+$(".save").on("click", function (event, this) {
+
+});
